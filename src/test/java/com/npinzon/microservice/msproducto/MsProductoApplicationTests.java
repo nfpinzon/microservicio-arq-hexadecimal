00c628bbbded7a15d6c339224b080d3f5a940557
Nelson F. Pinzon <nelsonfer25@gmail.com>
2024-04-07 11:16:47 -0500

    Se carga la estructura del proyecto

---
+++ src/test/java/com/npinzon/microservice/msproducto/MsProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npinzon.microservice.msproducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
